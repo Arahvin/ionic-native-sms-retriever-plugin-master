e590ecdff1e9de12e061a4b47ba7434e1047582d
Diogo Correia <diogo.manuel.correia@gmail.com>
2024-08-12 13:50:04 +0100

    Update AndroidSmsRetriever.java do get the sender address

--- src/android/com/codingsans/ionic/smsRetriever/AndroidSmsRetriever.java
+++ src/android/com/codingsans/ionic/smsRetriever/AndroidSmsRetriever.java
@@ -213,14 +213,19 @@ public class AndroidSmsRetriever extends CordovaPlugin {
           Status status = (Status) extra.get(SmsRetriever.EXTRA_STATUS);
           switch (status.getStatusCode()) {
             case CommonStatusCodes.SUCCESS:
+              try {
+                final String senderAddress = extra.getString("com.google.android.gms.auth.api.phone.EXTRA_SMS_ORIGINATING_ADDRESSS");
+              } catch(JSONException e) {}
               final String message = extra.getString(SmsRetriever.EXTRA_SMS_MESSAGE);
               //if (!StringUtils.hasContent(message)) return;
               if(message == null) return;
 
+              Log.d(TAG, senderAddress);
               Log.d(TAG, message);
 
               data = new JSONObject();
               try {
+                data.put("SenderAddress",senderAddress);
                 data.put("Message",message);
               } catch(JSONException e) {}
 
